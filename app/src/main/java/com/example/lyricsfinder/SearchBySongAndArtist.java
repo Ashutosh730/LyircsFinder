1a62fc425772bb391d2173dc4056c1322cbdc31b
Ashutosh Kumar <ashutoshhappy0007@gmail.com>
2021-02-03 12:07:17 +0530

    Now app can search the lyrics of song using song and artist name

--- app/src/main/java/com/example/lyricsfinder/SearchBySongAndArtist.java
+++ app/src/main/java/com/example/lyricsfinder/SearchBySongAndArtist.java
@@ -7,16 +7,33 @@ import androidx.core.content.ContextCompat;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SearchBySongAndArtist extends AppCompatActivity {
 
     private Button search;
     private TextInputEditText edtSong, edtArtist;
+    private TextView lyricsSong;
+
+    private String urlBase = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,7 @@ public class SearchBySongAndArtist extends AppCompatActivity {
         search = findViewById(R.id.searchSong);
         edtSong = findViewById(R.id.edtSong);
         edtArtist = findViewById(R.id.edtArtist);
+        lyricsSong = findViewById(R.id.lyricsSong);
 
         ActionBar actionBar;
         actionBar = getSupportActionBar();
@@ -40,6 +58,58 @@ public class SearchBySongAndArtist extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(ContextCompat.getColor(SearchBySongAndArtist.this, R.color.dark_blue));
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendAndRequestResponse();
+            }
+        });
+    }
 
+    private void sendAndRequestResponse() {
+
+
+        String url = urlBase;
+        /*
+        TODO Here is the api key for this app
+         */
+        url+="q_track="+edtSong.getText().toString()+"";
+        url+="&q_artist="+edtArtist.getText().toString()+"";
+        url+=key;
+
+        RequestQueue queue = Volley.newRequestQueue(SearchBySongAndArtist.this);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("TAG", response.toString());
+
+                try {
+                   String lyrics_body;
+                    JSONObject message = response.getJSONObject("message");
+                    JSONObject body = message.getJSONObject("body");
+                    JSONObject lyrics = body.getJSONObject("lyrics");
+                    lyrics_body = lyrics.getString("lyrics_body");
+                    lyricsSong.setText(lyrics_body);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("TAG", "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(jsonObjReq);
     }
 }
